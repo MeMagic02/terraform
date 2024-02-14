9446c6b619e406ff001465e22cf5527ccf7db75f
MeMagic02 <157368773+MeMagic02@users.noreply.github.com>
2024-02-14 12:50:11 +0100

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,8 +1,8 @@
 variable “cidr_block_RB_Public_Subnet1” {
-    default = “10.0.1.0/24"     #Public Subnet-1
+    default = “10.0.1.0/24"     
 }
 variable “CIDR_BLOCK” {
-    default = “0.0.0.0"         #Public CIDR
+    default = “0.0.0.0"        
 }
 variable “AWS_REGION” {
     default = “us-east-1"
