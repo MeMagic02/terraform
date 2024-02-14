592e6899e027ffbac39f1a2e0f85e5f000c8297b
MeMagic02 <157368773+MeMagic02@users.noreply.github.com>
2024-02-14 12:45:39 +0100

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,14 +1,14 @@
-# Code for running EC2 instance in Terraform
-resource “aws_instance” “instance” {
-  ami = “ami-0d442a425e2e0a743”
-  instance_type               = “t3.micro”
-  availability_zone           = “us-west-2a”
+resource "aws_instance" "instance" {
+  ami = "ami-0d442a425e2e0a743"
+  instance_type               = "t3.micro"
+  availability_zone           = "us-west-2a"
   associate_public_ip_address = true
-  key_name                    = “vockey”
-  vpc_security_group_ids      = [“sg-0c63e7d2130251897”]
-  subnet_id                   = “subnet-0843618c695a9e548"
-  count = 1
+  key_name                    = "vockey"
+  vpc_security_group_ids      = ["sg-0c63e7d2130251897"]
+  subnet_id                   = "subnet-0843618c695a9e548"
+  # iam_instance_profile        = "LabRole"
+  count = 2
   tags = {
-    Name = “first-try”
+    Name = "Sandbox1"
   }
 }
