5c26985fc21efb3e2489563eb31708d2a5acc9af
MeMagic02 <memagicayman@gmail.com>
2024-02-23 16:07:47 +0100

    create public subnet 2

---
+++ PublicSubnet2.tf
@@ -0,0 +1,11 @@
+# Create Public Subnet 2
+resource "aws_subnet" "public-2" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "deham9"
+  }
+}
