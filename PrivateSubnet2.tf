1f6773b0d1d919782b225c45c952d61ee22ccc4e
MeMagic02 <memagicayman@gmail.com>
2024-02-23 16:44:55 +0100

    adjusting the az

--- PrivateSubnet2.tf
+++ PrivateSubnet2.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "private-2" {
   vpc_id     = aws_vpc.dev_vpc.id 
   cidr_block = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-west-2b"
 
   tags = {
     Name = "PrivateSubnet2" 
