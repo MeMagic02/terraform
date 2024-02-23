fdd7a8d9aa2671f9eca47a9fce54738e31c61ead
MeMagic02 <memagicayman@gmail.com>
2024-02-23 17:10:09 +0100

    Asso Private Subnet 1

---
+++ PrivateSubnet1Asso.tf
@@ -0,0 +1,6 @@
+
+resource "aws_route_table_association" "Private_Subnet1_Asso" {
+  route_table_id = aws_route_table.RB_Private_RouteTable.id
+  subnet_id      = aws_subnet.private-1.id
+  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-1]
+}
