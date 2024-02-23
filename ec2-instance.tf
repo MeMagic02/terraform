79d0d505dc00f18957e61d01ba667e8193d2aacf
MeMagic02 <memagicayman@gmail.com>
2024-02-23 15:36:07 +0100

    updating subnetid and sgid

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "instance" {
   availability_zone           = "us-west-2a"
   associate_public_ip_address = true
   key_name                    = "vockey"
-  vpc_security_group_ids      = ["sg-02a34f5d2b5da44d8"]
-  subnet_id                   = "subnet-08ad54bd2568f99a1"
+  vpc_security_group_ids      = ["sg-0e40ede16b3a737d0"]
+  subnet_id                   = "subnet-04d204c4fe2d0a156"
   # iam_instance_profile        = "LabRole"
   count = 2
   tags = {
