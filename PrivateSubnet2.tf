ba8afa42116fbfc0cc8d146ce7869ced8aad2f38
MeMagic02 <memagicayman@gmail.com>
2024-02-23 16:38:41 +0100

    Create Private Subnet 2

---
+++ PrivateSubnet2.tf
@@ -0,0 +1,11 @@
+# Create Private Subnet 2
+esource "aws_subnet" "private-2" {
+  vpc_id     = aws_vpc.dev_vpc.id 
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "PrivateSubnet2" 
+  }
+}
+
