5ae246ecfa99af0cb334de7ed93ae3dc25a4ea05
MeMagic02 <memagicayman@gmail.com>
2024-02-23 17:11:56 +0100

    Asso Public and Private 2

---
+++ Public&Private2SubnetAsso.tf
@@ -0,0 +1,12 @@
+Asso Public and Private Subnet
+resource "aws_route_table_association" "Public_Subnet2_Asso" {
+  route_table_id = aws_route_table.RB_Public_RouteTable.id
+  subnet_id      = aws_subnet.public-2.id
+  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-2]
+}
+
+resource "aws_route_table_association" "Private_Subnet2_Asso" {
+  route_table_id = aws_route_table.RB_Private_RouteTable.id
+  subnet_id      = aws_subnet.private-2.id
+  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-2]
+}
