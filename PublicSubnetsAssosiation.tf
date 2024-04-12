3c526bdc496def46584a8b2815486eceeec98b58
MeMagic02 <memagicayman@gmail.com>
2024-04-12 17:13:55 +0200

    ps2

--- PublicSubnetsAssosiation.tf
+++ PublicSubnetsAssosiation.tf
@@ -5,8 +5,4 @@ resource "aws_route_table_association" "Public_Subnet1_Asso" {
   depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-1]
 }
 
-resource "aws_route_table_association" "Public_Subnet2_Asso" {
-  route_table_id = aws_route_table.RB_Public_RouteTable.id
-  subnet_id      = aws_subnet.public-2.id
-  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-2]
-}
+
