2e3fb90fcee36eba4af0b41e4bfb7b734dbd29e0
MeMagic02 <memagicayman@gmail.com>
2024-04-12 16:47:31 +0200

    del pub subn2

--- PublicSubnets.tf
+++ PublicSubnets.tf
@@ -10,14 +10,3 @@ resource "aws_subnet" "public-1" {
   }
 }
 
-# Create Public Subnet 2
-resource "aws_subnet" "public-2" {
-  vpc_id     = aws_vpc.dev_vpc.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "us-west-2b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "PublicSubnet2"
-  }
-}
